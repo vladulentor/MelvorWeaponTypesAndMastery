3ddb93015df83592e82a6d545750a4907c0c144d
vladulentor <mihailvladutpopescu@gmail.com>
2026-03-29 18:12:28 +0300

    Finished merge

--- WTM/src/patches/modPatches/showItemSourcesAndUses.mjs
+++
@@ -1,3 +0,0 @@
-export function showItemSourcesAndUsesCompatibility(ctx){
-    //shobon
-}
--- WTM/src/patches/patches/weaponMastery/raiseMasteryLevel.mjs
+++
@@ -1,8 +0,0 @@
-export function raiseMasteryLevel() {
-    for (const mastery of game.weaponMasteries.allObjects)
-        for (const sfixture of mastery.fixture)
-            if (sfixture.id === this.fixture.id)
-                {mastery.checkXP();
-                   game.bank.renderQueue.mastery = true;
-                }
-}
