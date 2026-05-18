1275e8a7d33ce414b46b9d270524917c51c2093c
vladulentor <mihailvladutpopescu@gmail.com>
2026-05-18 21:58:28 +0300

    Menu work and touchups, bug fixes, work on scimitars, other stuff

--- src/patches/patches/addFollowupSummonAttacks.mjs
+++ src/patches/patches/addFollowupSummonAttacks.mjs
@@ -2,11 +2,10 @@ export function addFollowupSummonAttacks({ patch }) { //hey you didnt capitalize
     patch(Player, "attack").after(function (dam, target, attack) {
         if (attack && attack.summonFollowAttacks > 0) {
             const player = this;
-            
             setTimeout(() => {
-                let count = attack.summonFollowAttacks;
+                let count = attack.summonFollowAttacks + this.game.modifiers.getValue("WTM:ExtraSummonHits", ModifierQuery.EMPTY);;
                 
-                while (count > 0) {
+                while (count > 0 && this.canSummonAttack) {
                     player.summonAttack();
                     count--;
                 }
