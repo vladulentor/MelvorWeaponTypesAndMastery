ce3318ca778f8746f8b0b689379fe600672a2cea
vladulentor <mihailvladutpopescu@gmail.com>
2026-07-21 07:50:41 +0300

    Fix fear-related bug

--- src/patches/patches/patchFearLikeExtension.mjs
+++ src/patches/patches/patchFearLikeExtension.mjs
@@ -1,11 +1,12 @@
-export function patchFearLikeExtension(){
-const ourB = game.combatEffects.getObjectByID("WTM:Terrify").behaviours[4]; // this may be stupid
-game.combatEffects.allObjects.forEach(effect => {
+export function patchFearLikeExtension() {
+    const ourB = game.combatEffects.getObjectByID("WTM:Terrify").behaviours[4]; // this may be stupid
 
-    effect.exclusiveGroups.forEach(exclusive => {
-        if (exclusive._localID == "Fear" && effect._localID!=="Terrify")
-        {effect.behaviours.push(ourB)}
-    })
-})
+
+    game.combatEffects.allObjects.forEach(effect => {
+        if (effect.effectGroups?.some(ef => ef._localID == "Fear") && effect.namespace !== "WTM") {
+
+            effect.behaviours.push(ourB)
+        }
+   })
 
 }
